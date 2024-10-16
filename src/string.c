8e4946a80193c4009161f6fb5d1483e9664d40c5
LowByteFox <firethefox77@gmail.com>
2024-10-16 21:57:19 +0200

    fix strcmp

--- src/string.c
+++ src/string.c
@@ -127,11 +127,11 @@ size_t strlen(const char *s)
 
 int strcmp(const char *s1, const char *s2)
 {
-    while (*s1 != *s2++)
-        if (*s1++ == 0)
-            return 0;
+	while (*s1 == *s2++)
+		if (*s1++ == 0)
+			return 0;
 
-    return (*(const uint8_t*)s1 - *(const uint8_t*)(s2 - 1));
+	return (*(unsigned char *)s1 - *(unsigned char *)--s2);
 }
 
 int strncmp(const char *s1, const char *s2, size_t n)
