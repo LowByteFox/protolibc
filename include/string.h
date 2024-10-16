70ff14a0995725eba75b283e067d0c8b891eff22
LowByteFox <firethefox77@gmail.com>
2024-10-16 21:48:14 +0200

    add more string functions, not implemented yet

--- include/string.h
+++ include/string.h
@@ -6,34 +6,41 @@
 
 __BEGIN_DECLS
 
-void *memcpy(void *dst, void *src, long n);
-void *memmove(void *dst, void *src, long n);
+void *memcpy(void *dst, const void *src, size_t n);
+void *rmemcpy(void *dst, const void *src, size_t n);
+void *memmove(void *dst, const void *src, size_t n);
 void *memset(void *s, int v, size_t n);
-void *memchr(void *s, int c, long n);
+void *memchr(const void *s, int c, size_t n);
 void *memrchr(void *s, int c, long n);
-int memcmp(char *s1, char *s2, long n);
+int memcmp(const void *s1, const void *s2, size_t n);
 
-char *strcpy(char *dst, char *src);
+char *strcpy(char *dst, const char *src);
 char *strchr(const char *s, int c);
-char *strrchr(char *s, int c);
+char *strchrnul(const char *s, int ch, size_t n);
+char *strrchr(const char *s, int ch);
 size_t strlen(const char *s);
-int strcmp(char *s1, char *s2);
+int strcmp(const char *s1, const char *s2);
+char *stpcpy(char *dst, const char *srt);
 
-char *strncpy(char *d, char *s, long n);
-char *strcat(char *d, char *s);
-int strncmp(char *d, char *s, long n);
-char *strstr(char *s, char *r);
+size_t strspn(const char *s, const char *accept);
+size_t strcspn(const char *s, const char *reject);
+char *strpbrk(const char *s, const char *accept);
+char *strsep(char **stringp, const char *delim);
+char *strtok(char *s, const char *delim);
+
+char *strncpy(char *d, const char *s, size_t n);
+char *strcat(char *d, const char *s);
+char *strncat(char *dest, const char *src, size_t n);
+int strncmp(const char *s1, const char *s2, size_t n);
+char *strstr(const char *s, const char *r);
+char *strerror(int errnum);
 
 char *strdup(const char *s);
+char *strndup(const char *s, size_t n);
 
 /* BSD related string function */
 size_t strlcpy(char *dst, const char *src, size_t dstsize);
 size_t strlcat(char *dst, const char *src, size_t dstsize);
-
-/* faster implementations */
-#define memchr(s, c, n)		__memchr_c(s, c, n)
-
-void *__memchr_c(void *s, int c, long n);
 
 __END_DECLS
 
--- src/string.c
+++ src/string.c
@@ -1,176 +1,186 @@
+#include <stdint.h>
 #include <string.h>
 
-#define LONG01	(0x01010101ul | (0x01010101ul << (sizeof(long) * 8 - 32)))
-#define LONG80	(0x80808080ul | (0x80808080ul << (sizeof(long) * 8 - 32)))
-#define LONGOFF(s)	((unsigned long) s & (sizeof(long) - 1))
-#define HASZERO(x)	(((x) - LONG01) & ~(x) & LONG80)
-
-void *__memchr_c(void *src, int c, long n)
+void *memcpy(void *dst, const void *src, size_t n)
 {
-	unsigned char *s = src;
-	c = (unsigned char) c;
-	unsigned long k = LONG01 * c;
-	while (LONGOFF(s) && n && *s != c)
-		s++, n--;
-	if (n && *s != c) {
-		unsigned long *w = (void *) s;
-		while (n >= sizeof(long) && !HASZERO(*w ^ k))
-			w++, n -= sizeof(long);
-		s = (void *) w;
-	}
-	while (n && *s != c)
-		s++, n--;
-	return n ? (void *) s : 0;
-}
-
-void *memcpy(void *dst, void *src, long n) {
-    unsigned char *dst_ptr = (unsigned char *)dst;
-    const unsigned char *src_ptr = (const unsigned char *)src;
+    uint8_t *d = dst;
+    const uint8_t *s = src;
 
     while (n--) {
-        *dst_ptr++ = *src_ptr++;
+        *d++ = *s++;
     }
 
     return dst;
 }
 
-void *memmove(void *dst, void *src, long n) {
-    unsigned char *dst_ptr = (unsigned char *)dst;
-    const unsigned char *src_ptr = (const unsigned char *)src;
+void *memmove(void *dst, const void *src, size_t n)
+{
+    uint8_t *d = dst;
+    const uint8_t *s = src;
 
-    if (src_ptr < dst_ptr && src_ptr + n > dst_ptr) {
-        dst_ptr += n;
-        src_ptr += n;
+    if (d < s) {
         while (n--) {
-            *--dst_ptr = *--src_ptr;
+            *d++ = *s++;
         }
     } else {
+        d += n;
+        s += n;
         while (n--) {
-            *dst_ptr++ = *src_ptr++;
+            *--d = *--s;
         }
     }
 
     return dst;
 }
 
-void *memset(void *s, int v, size_t n) {
-    unsigned char* p = (unsigned char*)s;
+void *memset(void *s, int v, size_t n)
+{
+    uint8_t* p = s;
 
     while (n--) {
-        *p++ = (unsigned char)v;
+        *p++ = (uint8_t) v;
     }
+
     return s;
 }
 
-void *memrchr(void *s, int c, long n) {
-    unsigned char *p = (unsigned char *)s + n - 1;
-    while (n-- > 0) {
-        if (*p == (unsigned char)c) {
-            return (void *)p;
-        }
-        p--;
+void *memrchr(void *s, int c, long n)
+{
+    const uint8_t *p;
+
+    if (n != 0) {
+        p = ((uint8_t*) s) + n;
+        do {
+            if (*(--p) == (uint8_t) c)
+                return (void*) p;
+        } while (--n != 0);
     }
+
     return NULL;
 }
 
-int memcmp(char *s1, char *s2, long n) {
-    unsigned char *p1 = (unsigned char *)s1;
-    unsigned char *p2 = (unsigned char *)s2;
-    while (n-- > 0) {
+int memcmp(const void *s1, const void *s2, size_t n)
+{
+    const uint8_t *p1 = s1;
+    const uint8_t *p2 = s2;
+
+    while (n--) {
         if (*p1 != *p2) {
             return *p1 - *p2;
         }
+
         p1++;
         p2++;
     }
+
     return 0;
 }
 
-char *strcpy(char *dst, char *src) {
+char *strcpy(char *dst, const char *src)
+{
     char *start = dst;
-    while (*src != '\0') {
-        *dst = *src;
-        dst++;
-        src++;
-    }
-    *dst = '\0';
+
+    for (; (*dst = *src); ++dst, ++src);
+
     return start;
 }
 
-char *strchr(const char *s, int c) {
-    while (*s != '\0') {
+char *strchr(const char *s, int c)
+{
+    while (*s) {
         if (*s == c) {
             return (char*) s;
         }
         s++;
     }
-    if (c == '\0') {
+
+    if (c == 0) {
         return (char*) s;
     }
+
     return NULL;
 }
 
-char *strrchr(char *s, int c) {
-    char *last = NULL;
-    while (*s != '\0') {
-        if (*s == c) {
-            last = s;
-        }
-        s++;
+char *strrchr(const char *s, int ch)
+{
+    char c, *save;
+
+    c = ch;
+    for (save = NULL;; ++s) {
+        if (*s == c)
+            save = (char *) s;
+        if (*s == 0)
+            return save;
     }
-    if (c == '\0') {
-        return s;
-    }
-    return last;
 }
 
-size_t strlen(const char *s) {
+size_t strlen(const char *s)
+{
     size_t length = 0;
-    while (*s != '\0') {
+    while (*s) {
         length++;
         s++;
     }
+
     return length;
 }
 
-int strcmp(char *s1, char *s2) {
-    while (*s1 != '\0' && *s2 != '\0') {
-        if (*s1 < *s2) {
-            return -1;
-        } else if (*s1 > *s2) {
-            return 1;
-        }
-        s1++;
-        s2++;
-    }
-    if (*s1 == '\0' && *s2 == '\0') {
-        return 0;
-    } else if (*s1 == '\0') {
-        return -1;
-    } else {
-        return 1;
-    }
+int strcmp(const char *s1, const char *s2)
+{
+    while (*s1 != *s2++)
+        if (*s1++ == 0)
+            return 0;
+
+    return (*(const uint8_t*)s1 - *(const uint8_t*)(s2 - 1));
 }
 
-int strncmp(char *d, char *s, long n) {
-    while (n > 0 && *d != '\0' && *s != '\0') {
-        if (*d < *s) {
-            return -1;
-        } else if (*d > *s) {
-            return 1;
-        }
-        d++;
-        s++;
-        n--;
-    }
-    if (n > 0) {
-        if (*d == '\0') {
-            return -1;
-        } else if (*s == '\0') {
-            return 1;
-        }
-    }
+int strncmp(const char *s1, const char *s2, size_t n)
+{
+    if (n == 0)
+        return 0;
+
+    do {
+        if (*s1 != *s2++)
+            return *(uint8_t*) s1 - *(uint8_t*) --s2;
+        if (*s1++ == 0)
+            break;
+    } while (--n != 0);
+
     return 0;
+}
+
+size_t strspn(const char *s, const char *accept)
+{
+    const char *p = s, *spanp;
+    char c, sc;
+    
+cont:
+    c = *p++;
+    for (spanp = accept; (sc = *spanp++) != 0;)
+        if (sc == c)
+            goto cont;
+
+    return p - 1 - s;
+}
+
+size_t strcspn(const char *s, const char *reject)
+{
+	const char *p, *spanp;
+	char c, sc;
+
+	for (p = s;;) {
+		c = *p++;
+		spanp = reject;
+		do {
+			if ((sc = *spanp++) == c)
+				return p - 1 - s;
+		} while (sc != 0);
+	}
+}
+
+char *strerror(int errnum)
+{
+    return NULL;
 }
 
 size_t strlcpy(char *dst, const char *src, size_t dstsize)
